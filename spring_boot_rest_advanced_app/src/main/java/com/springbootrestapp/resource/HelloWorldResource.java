a5aa6ca6d17153e0f4eb7244677374ad91fccb4e
sdaundka <sagardaundkar007@gmail.com>
2021-01-09 13:50:22 +0530

    Hello World Resource Update along with application.properties update

--- spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/resource/HelloWorldResource.java
+++ spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/resource/HelloWorldResource.java
@@ -1,5 +1,7 @@
 package com.springbootrestapp.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,11 @@ public class HelloWorldResource {
 	@RequestMapping(method = RequestMethod.GET, path = "/hello")
 	public String sayHelloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/hello/{user}")
+	public String sayHelloToUser(@PathVariable String user) {
+		return "Hello , "+user;
 	}
 	
 }
