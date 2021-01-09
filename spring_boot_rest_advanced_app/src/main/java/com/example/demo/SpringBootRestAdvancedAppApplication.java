a3ba228ed48d09374cd88648cc2617523218f641
sdaundka <sagardaundkar007@gmail.com>
2021-01-09 11:55:14 +0530

    initial commit

---
+++ spring_boot_rest_advanced_app/src/main/java/com/example/demo/SpringBootRestAdvancedAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestAdvancedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestAdvancedAppApplication.class, args);
+	}
+
+}
