5788212d5d5fe2997ec7b05072bd799613e33c03
sdaundka <sagardaundkar007@gmail.com>
2021-01-10 12:43:49 +0530

    i18n Resource and Configurations with Locale settings from default and Request Header

--- spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/SpringBootRestAdvancedAppApplication.java
+++ spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/SpringBootRestAdvancedAppApplication.java
@@ -1,13 +1,34 @@
 package com.springbootrestapp;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class SpringBootRestAdvancedAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestAdvancedAppApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+	
+	@Bean
+	public MessageSource bundleMessageSource() {
+		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasename("messages");
+		return resourceBundleMessageSource;
 	}
 
 }
---
+++ spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/resource/i18n/HelloUseri18nResource.java
@@ -0,0 +1,31 @@
+package com.springbootrestapp.resource.i18n;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/i18n")
+public class HelloUseri18nResource {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	@GetMapping
+	public String greetUser() {
+		String greetingMessage = messageSource.getMessage("greeting", null, Locale.US);
+		return greetingMessage;
+	}
+	
+	@GetMapping("/header")
+	public String greetUserWithRequestHeader(@RequestHeader(value = "Accept-Language", defaultValue = "us") Locale locale) {
+		String greetingMessage = messageSource.getMessage("greeting", null, locale);
+		return greetingMessage;
+	}
+	
+}
