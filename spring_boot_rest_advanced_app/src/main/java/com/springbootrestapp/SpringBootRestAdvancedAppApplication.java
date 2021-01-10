6199915fdb32682440a3d1a85e3176fd569f7ab7
sdaundka <sagardaundkar007@gmail.com>
2021-01-10 12:52:33 +0530

    i18n support by taking locale from header without using @RequestHeader annotation. Instead used LocaleContextHolder

--- spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/SpringBootRestAdvancedAppApplication.java
+++ spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/SpringBootRestAdvancedAppApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -17,9 +18,19 @@ public class SpringBootRestAdvancedAppApplication {
 		SpringApplication.run(SpringBootRestAdvancedAppApplication.class, args);
 	}
 	
+	/* 
+	 * This can be used if the default Locale or the Locale from the Request Header needs to be used using @RequestHeader
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+	*/
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver sessionLocaleResolver = new AcceptHeaderLocaleResolver();
 		sessionLocaleResolver.setDefaultLocale(Locale.US);
 		return sessionLocaleResolver;
 	}
--- spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/resource/i18n/HelloUseri18nResource.java
+++ spring_boot_rest_advanced_app/src/main/java/com/springbootrestapp/resource/i18n/HelloUseri18nResource.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,13 @@ public class HelloUseri18nResource {
 	
 	@GetMapping("/header")
 	public String greetUserWithRequestHeader(@RequestHeader(value = "Accept-Language", defaultValue = "us") Locale locale) {
+		String greetingMessage = messageSource.getMessage("greeting", null, locale);
+		return greetingMessage;
+	}
+	
+	@GetMapping("/nonheader")
+	public String greetUserWithoutRequestHeaderAnnotation() {
+		Locale locale = LocaleContextHolder.getLocale();
 		String greetingMessage = messageSource.getMessage("greeting", null, locale);
 		return greetingMessage;
 	}
